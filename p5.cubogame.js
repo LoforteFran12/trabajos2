494c8f291ecdbe1ef7b84464c01b7b0ce9c374d2
LoforteFran12 <d480670@alumnos.itr.edu.ar>
2024-05-24 09:25:12 -0300

    vamos bien

--- p5.cubogame.js
+++ p5.cubogame.js
@@ -1,23 +1,34 @@
 let ad = 370
 let h = -100
+let vidas = 3
+let puntos = 0
+let y = -30
+let x  
 
 function setup() {
   createCanvas(800, 600);
-  
+  fill(random(255),random(255),random(255));
 
 }
 
 function draw() {
-  background(240);
+  background(220);
   text('vidas=3', 670, 60);
    text('Puntos=0', 670, 30);
   square(ad, 498, 50);
-  fill(213,155,0);
+ 
+ ellipse(10, y, 40);
+ y++
+ if(y > 600){
+   
+    y = -30;
+   
+    }
   
 if(ad > 690){
    ad -=10
    }
-  if(ad > 0){
+  if(ad < 0){
    ad =+10
    }
 }
@@ -25,8 +36,8 @@ if(ad > 690){
 function keyPressed() 
 {
   if (key === 'a') {
-     ad-=10;
+     ad-=20;
   } else if (key === 'd') {
-    ad+=10;
+    ad+=20;
   }
 }
